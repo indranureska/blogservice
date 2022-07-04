97cecf54c7a97e3396c2eafe8ae74d99947be21a
Indra Nureska <indra.nureska@gmail.com>
2022-07-05 05:01:11 +0800

    code update

--- rest/common/ServiceConst.go
+++ rest/common/ServiceConst.go
@@ -6,4 +6,4 @@ const CREATE_USER_SERVICE_PATH = "/create-user"
 const UPDATE_USER_SERVICE_PATH = "/update-user"
 const DELETE_USER_SERVICE_PATH = "/delete-user/{id:[a-zA-Z0-9]*}"
 
-const BLOG_DB_URI = "mongodb://127.0.0.1:27017"
+const BLOG_DB_URI = "mongodb://127.0.0.1:27018"
---
+++ rest/function/BlogPostService.go
@@ -0,0 +1,17 @@
+package function
+
+import "net/http"
+
+// TODO: post
+func Post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+}
+
+// TODO: update
+
+// TODO: delete
+
+// TODO: publish
+
+// TODO: post and publish
